799a0396f1ee4e298a01412b9c4fb669846ba1cb
qinqq <qinqq@sssyin.cn>
2018-12-28 17:00:42 +0800

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,74 @@
+const dataInfo = {
+    meta:{
+        "success": true,
+        "message": "ok"
+    },
+    data:{
+	    "bannerList":[
+	    	{
+	    		"id":"12",
+	    		"imgUrl":"../.././img/timg.jpg"
+	    	},
+	    	{
+	    		"id":"13",
+	    		"imgUrl":"../.././img/hose.jpg"
+	    	}
+	    ],
+	    "imageList":[
+	    	"../.././img/ee.jpg",
+	    	"../.././img/rr.jpg",
+	    ],
+	    "img":[
+	    	"../.././img/ee.jpg",
+	    	"../.././img/rr.jpg",
+	    ],
+	    "list": [
+	        {
+	            "id": "12",
+	            "name": "测试2",
+	            "area": "Sabah",
+	            "type": "1",
+	            "score": 8.2,
+	            "latitude": 30.244959,
+	            "longitude": 120.211239,
+	            "mainImg": "http://192.168.2.99:8081/tour/userfiles/1/images/3aa2fe509fcd4991a04c9b46c9696762.png",
+	            "logo": "http://192.168.2.99:8081/tour/userfiles/1/images/d5dc8312ef6147c4823fde6094613f73.jpg",
+	            "className": "海鲜",
+	            "averagePrice": "80",
+	            "isRecommend": "0",
+	            "distance": 1149630.6
+	        },
+	        {
+	            "id": "11",
+	            "name": "测试啦啦啦",
+	            "area": "Sabah",
+	            "type": "1",
+	            "score": 8,
+	            "latitude": 30.241758,
+	            "longitude": 120.211541,
+	            "mainImg": "http://192.168.2.99:8081/tour/userfiles/1/images/56a72d35b8404825befdee6a57722fc6.png",
+	            "logo": "http://127.0.0.1:8080/tour/userfiles/1/images/aa3d9d68e486423cba5d6bc35de58a8c.jpg",
+	            "className": "历史古迹",
+	            "averagePrice": "96",
+	            "isRecommend": "0",
+	            "distance": 1149812.1
+	        },
+	        {
+	            "id": "6",
+	            "name": "测试餐厅1",
+	            "area": "Sabah",
+	            "type": "1",
+	            "score": 9.2,
+	            "latitude": 23.099994,
+	            "longitude": 113.32452,
+	            "mainImg": "http://192.168.2.99:8081/tour/userfiles/1/images/e1dcef353f8b4710b09a47f643856d03.jpg",
+	            "logo": "http://192.168.2.99:8081/tour/userfiles/1/images/2874c41d959e44e9a7e944163e2549d8.jpg",
+	            "className": "马来西亚菜",
+	            "averagePrice": "80",
+	            "isRecommend": "0",
+	            "distance": 1313988.5
+	        }
+	    ]
+    }
+}
+export {dataInfo};
